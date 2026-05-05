39784bb1ef2e278152eb1e8ab69b9baddfa64112
Muhammad Unse <muhammadunse11@gmail.com>
2026-05-05 22:58:24 +0500

    Day2

---
+++ Backend/Day2/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/about',(req,res)=>{
+  res.send('This is the about page');
+});
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
